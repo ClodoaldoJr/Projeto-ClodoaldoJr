39665d164a218b8c4580bd2c7a7105023020f8e6
ClodoaldoJr <63881506+ClodoaldoJr@users.noreply.github.com>
2020-04-19 15:25:32 -0300

    Ajustes
    Slick

--- Orlando.js
+++ Orlando.js
@@ -12,3 +12,13 @@
 			});
 
 		});
+
+		$(".News-slider").slick({
+			infinite: true,
+			slidesToShow: 3,
+			slidesRoScroll: 1,
+			centerMode: true,
+			prevArrow: $("#arrow-prev"),
+			nextArrow: $("#arrow-next"),
+
+		});
